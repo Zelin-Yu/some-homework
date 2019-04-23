63d8f1f5db513a82793024173d5f1ddc1d96acf1
Zelin-Yu <33053040+Zelin-Yu@users.noreply.github.com>
2019-04-24 00:31:46 +0800

    Rename HW4-A-1-recursive.cpp to postfix2infix-recursive.cpp

--- postfix2infix-recursive.cpp
+++ postfix2infix-recursive.cpp
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-//���ȼ����壬���ȼ����������
-//_L:���ϣ�ͬ��������ߵ�����
+//优先级定义，优先级大的先运算
+//_L:左结合，同级运算左边的先算
 enum priority
 {
 	ADD, ADD_L, MUL, MUL_L
 };
 
 /*
-��ȡ��׺����ʽ
-�Ӻ���ǰ,���õ������opʱ,�ݹ��ȡ�����ӱ���ʽ�������
-subExp = leftExp op rightExp���ӱ���ʽ��Ҫ�ȼ���,��()��
-�Ƿ��ȼ���ͨ�����ȼ��ж�:�ݹ��ϲ�ı���ʽop'���ȼ���op��ͼ�()��
-����һ�ɲ���()
-����:��׺����ʽ(����,��ΪҪ�ı�)����ǰ���ȼ�(���Ǽ������ӱ���ʽ����_L)
-������׺����ʽ
+获取中缀表达式
+从后往前,当得到运算符op时,递归获取左右子表达式，结果是
+subExp = leftExp op rightExp。子表达式若要先计算,加()。
+是否先计算通过优先级判断:递归上层的表达式op'优先级比op大就加()。
+数字一律不加()
+参数:后缀表达式(引用,因为要改变)，当前优先级(若是计算右子表达式传入_L)
+返回中缀表达式
 */
 string exp(string &postfixExp, int priority = 0)
 {
 	int len = postfixExp.length();
 	char op = postfixExp[len - 1];
-	postfixExp = postfixExp.substr(0, len - 1);	//������ȥ����ĩһ���ַ�
+	postfixExp = postfixExp.substr(0, len - 1);	//处理后去掉最末一个字符
 
 	string rightExp, leftExp, subExp;
 	switch (op)
@@ -53,7 +53,7 @@ string exp(string &postfixExp, int priority = 0)
 	default:
 		string num;
 		num += op;
-		//���־����ӱ���ʽ
+		//数字就是子表达式
 		return num;
 		break;
 	}
